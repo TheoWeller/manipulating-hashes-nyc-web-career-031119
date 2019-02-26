abbacfc69b8fff8b487c0ad7522e19106c02af00
Theo Weller <weller.theodor@gmail.com>
2019-02-26 21:03:04 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-  contacts.delete_if {|key, value| key == favorite_icecream_flavors && value == "strawberry"}
+  contacts.delete_if {|key, value| key == favorite_icecream_flavors: && value == "strawberry"}
 
 
   #remember to return your newly altered contacts hash!
